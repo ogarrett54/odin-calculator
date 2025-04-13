f48900f79188f3f4fa67f552cc292b497490ecc3
Oscar <odgarrett@ucdavis.edu>
2025-04-13 13:09:50 -0700

    Sketch UI, skeleton javascript

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// Calculations
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+
+function equals() {}
+
+// DOM interaction
+function updateDisplay() {}
+
+function clear() {}
+
+function backspace() {}
+
+// Helpers
+function roundDecimals() {}
+
+function ridicule() {}
+
+// Selectors
+// Inputs
+
+// Operators
+
+// Deletors
